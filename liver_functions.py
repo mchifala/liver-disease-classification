1a23888377b1827f0ad97c2aba6b4a2481bdab63
mchifala <mich5456@colorado.edu>
2019-11-30 17:12:39 -0700

    Add REST server and client

--- liver_functions.py
+++ liver_functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import sklearn as sk
 import seaborn as sns
 from sklearn.model_selection import train_test_split
@@ -20,6 +22,31 @@ def import_data(csv_file):
     
     """
     return pd.read_csv(csv_file)
+
+def make_correlation_heatmap(df, title, outfile):
+    """
+    This function creates a correlation heatmap for a dataframe
+    of features and saves it to a user-defined file.
+    
+    Parameters:
+    - df(DataFrame): A dataframe of X features and y label
+    - title: The title of the correlation heat map
+    - outfile(str): The file where the heat map will be saved
+    
+    Returns:
+    - None, but a .png file is created 
+    
+    """
+    df_corr = df.corr()
+    plt.figure(figsize=(10, 10))
+    plt.title(title)
+    mask = np.zeros_like(df_corr)
+    mask[np.triu_indices_from(mask)]=True
+    with sns.axes_style("white"):
+        ax = sns.heatmap(df_corr, fmt=".2f", mask=mask, annot=True)
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+    plt.savefig(outfile)
 
 def one_hot_encode(df, col_label):
     """
